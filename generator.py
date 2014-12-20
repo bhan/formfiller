4ce8f9ba6733571e4ae46988e50e052c2c401ab4
Bo Ning Han <bo.ning.han@gmail.com>
2014-12-20 01:13:08 -0800

    add comment motivated by jordan

--- generator.py
+++ generator.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.platypus import Image, SimpleDocTemplate
 
 c = canvas.Canvas("output.pdf", pagesize=letter)
 c.setFont("Times-Roman", 14)
 width, height = letter
-c.drawString(100, 100, "Page 1")
+seal = Image("seal.png")
+seal.drawOn(c,100,500)
+c.drawString(100, 600, "Page 1")
 c.showPage() # next draws on new page
 c.drawString(100, 100, "Page 2")
 c.showPage()
 c.save()
+
+#TODO save pdf origin as image and overlay
+#things = []
+#things.append(Image("seal.png"))
+#doc = SimpleDocTemplate("output.pdf", pagesize=letter)
+#doc.build(things)
