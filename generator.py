4d61b9b90feedf21804c3d46124b6c3bf917b227
Bo Ning Han <bo.ning.han@gmail.com>
2014-12-20 00:07:15 -0800

    set font to times roman

--- generator.py
+++ generator.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
-c = canvas.Canvas("hello.pdf")
-c.drawString(100, 100, "Hello World")
+c = canvas.Canvas("output.pdf", pagesize=letter)
+c.setFont("Times-Roman", 14)
+width, height = letter
+c.drawString(100, 100, "Page 1")
+c.showPage() # next draws on new page
+c.drawString(100, 100, "Page 2")
 c.showPage()
 c.save()
