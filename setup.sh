1bf26917f2d04ac13112c3f1e4e30f97f705d463
Bo Ning Han <bo.ning.han@gmail.com>
2014-12-19 23:28:50 -0800

    run . ./setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-#virtualenv venv
-#source venv/bin/activate
-#pip install reportlab --upgrade
+# must be run using . ./setup.sh
+virtualenv venv
+source venv/bin/activate
+pip install reportlab --upgrade
